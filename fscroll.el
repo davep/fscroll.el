89994e2dc8a6d3272795035ecfcf9dc70e12fdde
Dave Pearson <davep@davep.org>
2017-03-23 16:33:27 +0000

    Add an actual description of why this file exists

--- fscroll.el
+++ fscroll.el
@@ -6,10 +6,17 @@
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/fscroll.el
 
-;;; Commentary:
-
 ;; fscroll.el is free software distributed under the terms of the GNU
 ;; General Public Licence, version 2. For details see the file COPYING.
+
+;;; Commentary:
+
+;; Out of the box GNU Emacs only scrolls the buffer when using `scroll-down'
+;; and `scroll-up' (which most people would have bound to PgDn and PgUp).
+;; Once the first or last lines are visible you'll get start/end of buffer
+;; errors if you try and hit those keys more. Many other editors will, when
+;; that happens, move the cursor to the start or end of the buffer. This
+;; code makes GNU Emacs do that too.
 
 ;;; Code:
 
